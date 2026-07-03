499d4762bd0fc9043a8b7aa715142be6d1e013c1
Hamid Muhammad Almuazzin <almuazzinhamid@gmail.com>
2026-07-03 21:58:14 +0100

    Made admission steps

---
+++ script.js
@@ -0,0 +1,46 @@
+const plus = document.querySelector(".plus");
+const phone = document.querySelector(".phone");
+const whatsapp = document.querySelector(".whatsapp");
+const venue = document.querySelector(".location");
+const btnDiv = document.querySelector(".btnsDiv");
+const cover = document.querySelector(".cover")
+
+plus.addEventListener("click", () => {
+    if (cover.classList.contains("active")){
+        plus.classList.toggle("active")
+        setTimeout(() => {
+            phone.classList.toggle("active")
+            setTimeout(() => {
+                whatsapp.classList.toggle("active")
+                setTimeout(() => {
+                    venue.classList.toggle("active")
+                    setTimeout(() => {
+                        btnDiv.classList.toggle("active")
+                        setTimeout(() => {
+                            cover.classList.toggle("active") 
+                        }, 500);
+                    }, 200);
+                }, 200);
+            }, 200);
+        }, 200);
+    } else{
+        cover.classList.toggle("active")    
+        setTimeout(() => {
+            btnDiv.classList.toggle("active")
+            setTimeout(() => {
+                plus.classList.toggle("active")
+                setTimeout(() => {
+                    phone.classList.toggle("active")
+                    setTimeout(() => {
+                        whatsapp.classList.toggle("active")
+                        setTimeout(() => {
+                            venue.classList.toggle("active")
+                        }, 200);
+                    }, 200);
+                }, 200);
+                
+            }, 200);
+        }, 400);
+    }
+
+})
